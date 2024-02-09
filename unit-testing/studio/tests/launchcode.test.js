b97b20f5085372f62af6cb7df10455ae200fda84
Tim Jones <tjones.2089@gmail.com>
2024-02-08 20:37:11 -0600

    Launchcode studio test

--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,37 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("Object keys match values", () => {
+    expect(launchcode.organization).toBe("nonprofit");
+    expect(launchcode.executiveDirector).toBe("Jeff");
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
 
-  // Write your unit tests here!
+  test("Check if array containsWeb Web Development, Data Analysis, Liftoff", () => {
+    expect(launchcode.programsOffered.includes("Web Development")).toBe(true);
+    expect(launchcode.programsOffered.includes("Data Analysis")).toBe(true);
+    expect(launchcode.programsOffered.includes("Liftoff")).toBe(true);
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test("When passed a number that is ONLY divisible by 2 return 'Launch!'", () => {
+    expect(launchcode.launchOutput(2)).toBe("Launch!")
+  });
+
+  test("When passed a number that is ONLY divisible by 3, return 'Code!'", () => {
+    expect(launchcode.launchOutput(3)).toBe("Code!")
+  });
+
+  test("When passed a number that is ONLY divisible by 5, return 'Rocks!'", () => {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!")
+  });
+
+  test("When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'", () => {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!")
+  });
+  
+  test("When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'", () => {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!")
+  });
+  
   
 });
