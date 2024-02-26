f31d45cf9801de10026c3a8d357a938d1aa7eb51
Tim Jones <timjones.2089@gmail.com>
2024-02-26 12:13:26 -0600

    DOM exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,27 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener('click', event => {
+       paragraph.innerHTML = 'Houston! We have liftoff!'; 
+    });
+    
+    missionAbort.addEventListener('mouseover', () => {
+        missionAbort.style.backgroundColor = 'red';
+    });
+    
+    missionAbort.addEventListener('mouseout', () => {
+        missionAbort.style.backgroundColor = '';
+    });
+
+    function confirmationWindow() {
+      let message = missionAbort.addEventListener(onclick).confirm("Are you sure you want to abort the mission?");
+       if (message == true){
+        missionAbort.innerHTML = "Mission aborted! Space shuttle returning home"
+       } else {
+        missionAbort.innerHTML = "";
+       };
+     }
+
     
 }
 
